d08a3168c4f47fe95bf7f2d77c7f51e725489810
Mariette Olwagen <90972322+MarietteO@users.noreply.github.com>
2025-08-06 02:20:24 +0200

    Create zzExtrapractice.py

---
+++ zzExtrapractice.py
@@ -0,0 +1,18 @@
+#Find leap year
+
+def is_leap_year(year):
+    if year%4 == 0:
+        if year%100 == 0:
+            if year%400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+            
+if is_leap_year(2024):
+     print("True")
+else:
+    print("False")
